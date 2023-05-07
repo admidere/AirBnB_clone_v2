9d323762672f54845957fefd170cfd2bc61f85d0
admidere <admasud89@gmail.com>
2023-05-07 07:17:01 -0700

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -2,39 +2,53 @@
 """ A fabric script that generates a .tgz archive from the contents
     of the web_static folder and deploys it to web servers.
 """
-from fabric.api import local, put, run, env
-from datetime import datetime
-import os
+
+
+import os.path
+from fabric.api import env, put, run
 
 
 env.hosts = ['18.207.2.134', '100.25.152.180']
+env.user = 'ubuntu'
+# env.key_filename = '<path_to_ssh_key>'
 
 
 def do_deploy(archive_path):
-    if not os.path.exists(archive_path):
+    """Distributes an archive to web servers"""
+    if not os.path.isfile(archive_path):
         return False
 
     try:
-        archive_name = os.path.basename(archive_path)
-        archive_name_no_ext = archive_name.split('.')[0]
-        remote_path = "/data/web_static/releases/{}".format(archive_name_no_ext)
-
         # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/{}".format(archive_name))
+        put(archive_path, "/tmp/")
 
-        # Uncompress the archive to the folder on the web server
-        run("mkdir -p {}".format(remote_path))
-        run("tar -xzf /tmp/{} -C {}".format(archive_name, remote_path))
+        # Uncompress the archive to the folder
+        # /data/web_static/releases/<archive filename without extension> on the web server
+        archive_name = os.path.basename(archive_path)
+        archive_dir = archive_name.split(".")[0]
+        run("mkdir -p /data/web_static/releases/{}/".format(archive_dir))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
+            .format(archive_name, archive_dir))
 
         # Delete the archive from the web server
         run("rm /tmp/{}".format(archive_name))
 
-        # Delete the symbolic link /data/web_static/current from the webserver
-        run("rm -f /data/web_static/current")
+        # Move the contents of the web static directory to the parent directory
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/"
+            .format(archive_dir, archive_dir))
 
-        # Create a new symbolic link on the web server
-        run("ln -s {} /data/web_static/current".format(remote_path))
+        # Remove the web static directory
+        run("rm -rf /data/web_static/releases/{}/web_static".format(archive_dir))
 
+        # Delete the symbolic link /data/web_static/current from the web server
+        run("rm -rf /data/web_static/current")
+
+        # Create a new symbolic link /data/web_static/current on the web server
+        # linked to the new version of your code (/data/web_static/releases/<archive filename without extension>)
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(archive_dir))
+
+        print("New version deployed!")
         return True
+
     except Exception:
         return False
