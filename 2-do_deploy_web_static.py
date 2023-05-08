24ededd89f201eafc90f9c15de3de029a2cbcbb5
admidere <111120975+admidere@users.noreply.github.com>
2023-05-08 06:42:20 -0700

    deploy we static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,40 +16,38 @@ def do_deploy(archive_path):
     if not os.path.exists(archive_path):
         return False
 
-    archive_name = os.path.basename(archive_path)
-    archive_dir = os.path.splitext(archive_name)[0]
+   file_name = os.path.basename(archive_path)
+   archive_name = os.path.splitext(file_name)[0]
 
     try:
         # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/")
+        put(archive_path, '/tmp/')
+
+        # Create the directory to uncompress the archive
+        run('mkdir -p /data/web_static/releases/{}/'.format(archive_name))
 
         # Uncompress the archive to the folder
-        # /data/web_static/releases/<archive filename without extension>
-        # on the web server
-        run("mkdir -p /data/web_static/releases/{}/".format(archive_dir))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/"
-            .format(archive_name, archive_dir))
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(file_name, archive_name))
 
         # Delete the archive from the web server
-        run("rm /tmp/{}".format(archive_name))
+        run('rm /tmp/{}'.format(file_name))
 
-        # Move the contents of the web static directory tothe parent directory
+        # Move the files to the new folder and remove the old folder
         run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(archive_dir, archive_dir))
+            /data/web_static/releases/{}/'.format(archive_name,
+                                                  archive_name))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(archive_name))
 
-        # Remove the web static directory
-        run("rm -rf /data/web_static/releases/{}/web_static"
-            .format(archive_dir))
+        # Delete the symbolic link /data/web_static/current from the web
+        run('rm -rf /data/web_static/current')
 
-        # Delete the symbolic link /data/web_static/current from the webserver
-        run("rm -rf /data/web_static/current")
+        # Create a new the symbolic link /data/web_static/current
+        run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(archive_name))
 
-        # Create a new symbolic link /data/web_static/current on the webserver
-        # linked to the new version of your code (/data/web_static/releas
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current"
-            .format(archive_dir))
-
-        print("New version deployed!")
+        print('New version deployed!')
         return True
 
     except Exception:
