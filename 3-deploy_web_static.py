1e3954721b0a3034f5f82deff820c7ae4faa7f31
admidere <admasud89@gmail.com>
2023-05-09 11:59:15 +0000

    full deployment

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ A fabric script that generates a .tgz archive from the contents
-    of the web_static folder.
+    of of the web_static folder.
 """
 from fabric.api import local, run, put, env
 import os
