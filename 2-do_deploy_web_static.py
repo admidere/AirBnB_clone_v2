71c3f89bff5a6c362720129fbbff49257535777c
admidere <admasud89@gmail.com>
2023-05-08 23:45:47 -0700

    deploy web static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -5,7 +5,7 @@
 
 
 from fabric.api import env, put, run
-from os.path import exists
+import os
 
 env.user = 'ubuntu'
 env.hosts = ['18.207.2.134', '100.25.152.180']
@@ -13,37 +13,44 @@ env.key_filename = '~/.ssh/id_rsa'
 
 
 def do_deploy(archive_path):
-    if not exists(archive_path):
+    """
+    Distributes an archive to the web servers
+    """
+    if not os.path.exists(archive_path):
         return False
 
-     # Get the archive name without its extension (.tgz)
-    archive_name = archive_path.split("/")[-1]
-    archive_name_no_extension = archive_name.split(".")[0]
-    # Uncompress the archive to
-    # /data/web_static/releases/archive_name_no_extension
-    destined_folder = "/data/web_static/releases/{}/"\
-                      .format(archive_name_no_extension)
+    filename = os.path.basename(archive_path)
+    name = os.path.splitext(filename)[0]
 
-    # Returns True if all operations have been done correctly,
-    # otherwise returns False
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, "/tmp/")
-        # Create a directory where it should be uncompressed
-        run("mkdir -p {}".format(destined_folder))
-        # Uncompress to the destined directory
-        run("tar -xzf /tmp/{} -C {}".format(archive_name, destined_folder))
-        # Delete the uploded archive (in /tmp/ directory)
-        run("rm /tmp/{}".format(archive_name))
-        # Move all files inside web_static to the destined_folder created
-        run("mv {}web_static/* {}".format(destined_folder, destined_folder))
-        # Delte the empty web_static folder
-        run("rm -rf {}web_static".format(destined_folder))
-        # Delete the symbolic link
-        run("rm -rf /data/web_static/current")
-        # Create a new symbolic link
-        run("ln -s {} /data/web_static/current".format(destined_folder))
-        print("New version deployed!")
+        """ Upload the archive to the /tmp/ directory of the web server"""
+        put(archive_path, '/tmp/')
+
+        """ Create the directory to uncompress the archive"""
+        run('mkdir -p /data/web_static/releases/{}/'.format(name))
+
+        """ Uncompress the archive to the folder"""
+        run('tar -xzf /tmp/{} -C /data/web_static/releases/{}/'
+            .format(filename, name))
+
+        """ Delete the archive from the web server"""
+        run('rm /tmp/{}'.format(filename))
+
+        """ Move the files to the new folder and remove the old folder"""
+        run('mv /data/web_static/releases/{}/web_static/* \
+            /data/web_static/releases/{}/'.format(name, name))
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(name))
+
+        """ Delete the symbolic link /data/web_static/current from the web"""
+        run('rm -rf /data/web_static/current')
+
+        """ Create a new the symbolic link /data/web_static/current"""
+        run('ln -s /data/web_static/releases/{}/ \
+            /data/web_static/current'.format(name))
+
+        print('New version deployed!')
         return True
-    except:
+
+    except Exception:
         return False
