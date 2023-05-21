64c16e297603119ac042e400464f22057ab635ec
admidere <admasud89@gmail.com>
2023-05-21 09:20:08 -0700

    updated

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+"""
+Start Flask application
+
+Your web application must be listening on 0.0.0.0, port 5000
+You must use storage for fetching data from the
+storage engine (FileStorage or DBStorage) => from models import storage
+and storage.all(...)
+After each request you must remove the current SQLAlchemy Session:
+Declare a method to handle @app.teardown_appcontext
+Call in this method storage.close()
+Routes:
+/states_list: display a HTML page: (inside the tag BODY)
+H1 tag: “States”
+UL tag: with the list of all State objects present
+in DBStorage sorted by name (A->Z) tip
+LI tag: description of one State: <state.id>: <B><state.name></B>
+Import this 7-dump to have some data
+You must use the option strict_slashes=False in your route definition
+"""
+
+from flask import Flask, render_template
+from models import storage
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Display an HTML page with the states listed in alphabetical order
+
+    H1 tag: "States"
+    UL tag: with the list of all State objects present
+    in DBStorage sorted by name (A->Z)
+    LI tag: description of one State: <state.id>: <B><state.name></B>
+    """
+    states = sorted(list(storage.all("State").values()), key=lambda x: x.name)
+    return render_template('7-states_list.html', states=states)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """Closes the storage on teardown"""
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
