f6060b03116392f169e679bcf791cc3fdf370a48
admidere <admasud89@gmail.com>
2023-05-21 13:42:47 -0700

    updated

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,31 +1,25 @@
 #!/usr/bin/python3
-"""Start web application with two routings
-"""
-
-from models import storage
-from models.state import State
+""" A script that lists states"""
 from flask import Flask, render_template
+from models import storage
+from models import State
+from operator import attrgetter
 app = Flask(__name__)
 
 
-@app.route('/states_list')
+@app.route('/states_list', strict_slashes=False)
 def states_list():
-    """Render template with states
-    """
-    path = '7-states_list.html'
-    states = storage.all(State)
-    # sort State object alphabetically by name
-    sorted_states = sorted(states.values(), key=lambda state: state.name)
-    return render_template(path, sorted_states=sorted_states)
+    """Display an HTML page"""
+    states = [obj for obj in storage.all().values() if isinstance(obj, State)]
+    states_sorted = sorted(states, key=attrgetter('name'))
+    return render_template('7-states_list.html', States=states_sorted)
 
 
 @app.teardown_appcontext
-def app_teardown(arg=None):
-    """Clean-up session
-    """
+def teardown_app_context(exception):
+    """It removes the current SQLAlchemy sessions after each requests"""
     storage.close()
 
 
 if __name__ == '__main__':
-    app.url_map.strict_slashes = False
     app.run(host='0.0.0.0', port=5000)
